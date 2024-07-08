adcda70ff3fe29e3430f68ea62f4592976e6fab6
changhyun-cra <hellow6044@gmail.com>
2024-07-08 16:39:16 +0900

    [refactoring] Modify ReadCommand 1. 상태: refactoring 2. 수정 내용: ReadCommnad validationCheck 추가 3. 추가된 (Test된 ) TC 명: Shell_Read_Execute_Fail 4. Unit Test 전체 Pass 여부: O

--- test-shell/ReadCommand.cpp
+++ test-shell/ReadCommand.cpp
@@ -1,5 +1,6 @@
 #include "command.h"
 #include <stdexcept>
+#include <regex>
 
 class ReadCommand : public ICommand {
 public:
@@ -7,12 +8,12 @@ public:
 
 	void injectSSD(ISSD& ssd) override
 	{
-
+		this->ssd = &ssd;
 	}
 
 	void excute(CommandArgs& args) override
 	{
-		if (args.addr < 0 || args.addr > 99) throw std::invalid_argument("");
+		validationCheck(args.addr);
 		args.value = ssd->read(args.addr);
 	}
 
@@ -23,10 +24,18 @@ public:
 
 private:
 	ISSD* ssd;
+	const int LBA_MIN_VAL = 0;
+	const int LBA_MAX_VAL = 99;
 	const string strHelp = "\
 		LBA에 적힌 값을 읽어 화면에 출력한다.\n \
 		[Example] read LBA\n \
 		[Parameters]\n \
 		- LBA: 값을 읽을 LBA 영역 값 (0~99)\n \
 		[Returns] LBA에서 읽은 데이터를 출력합니다.\n";
+
+	void validationCheck(int addr) {
+		if (addr < LBA_MIN_VAL || addr > LBA_MAX_VAL) {
+			throw std::invalid_argument("잘못된 LBA 값 입니다.");
+		}
+	}
 };
