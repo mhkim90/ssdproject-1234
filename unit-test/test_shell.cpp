1ccc58ea8e30af244f797a225c609403edb9bbde
juheon.song <bootplus@naver.com>
2024-07-08 16:13:38 +0900

    [refactoring] shell unit test fixutre 반영

--- unit-test/test_shell.cpp
+++ unit-test/test_shell.cpp
@@ -3,23 +3,52 @@
 #include "../test-shell/shell.cpp"
 
 
+using namespace std;
 using namespace testing;
 
 class MockCommandFactory : public ICommandFactory {
 public:
 };
 
-TEST(SHELL_TEST, WAIT_FOR_COMMAND_READ) {
+class ShellFixutre : public Test {
+public:
+	ShellFixutre()
+		: shell{ mockFactory }, _oldStreamBuf{ nullptr } {
+
+	}
+
+	void inputCommand(const string& inputStr) {
+		_stringStream.str(inputStr);
+		cin.set_rdbuf(_stringStream.rdbuf());
+	}
+
 	MockCommandFactory mockFactory;
-	Shell shell(mockFactory);
+	Shell shell;
 
-	std::istringstream stringStream("exit");
-	auto oldStreamBuf = cin.rdbuf();
-	cin.set_rdbuf(stringStream.rdbuf());
+protected:
+	void SetUp() override {
+		_oldStreamBuf = cin.rdbuf();
+	}
 	
-	string rst = shell.waitForCommand();
+	void TearDown() override {
+		cin.set_rdbuf(_oldStreamBuf);
+		cin.clear();
+	}
 
-	EXPECT_EQ(rst, "exit");
+private:
+	istringstream _stringStream;
+	streambuf* _oldStreamBuf;
+};
 
-	cin.set_rdbuf(oldStreamBuf);
+TEST_F(ShellFixutre, WAIT_FOR_COMMAND_READ_EXIT) {
+	inputCommand("exit");
+
+	EXPECT_EQ(shell.waitForCommand(), "exit");
+}
+
+
+TEST_F(ShellFixutre, WAIT_FOR_COMMAND_READ_HELP) {
+	inputCommand("help");
+
+	EXPECT_EQ(shell.waitForCommand(), "help");
 }
