c829941917725f3680643fed85cb1360b2072502
seulgiishin <tls940807@gmail.com>
2024-07-08 13:49:11 +0900

    [feature] Add Write Command Handler 1. 상태 (feature) 2. 수정/반영내용 : add ssd write command handler feature 3. 추가된 (Test된 ) TC 명 :    - SSDCommandTest/WriteTest    - SSDCommandTest/MockWriteTest 4. unit test 전체 Pass 여부 (O)

--- ssd/CmdHandler.h
+++ ssd/CmdHandler.h
@@ -1,11 +1,21 @@
-#include <stdio.h>
+#pragma once
+#include <string>
+
+using namespace std;
+
+struct DataSet {
+public:
+	int lba;
+	string data;
+};
 
 class CmdHandler
 {
 public:
+	CmdHandler() {};
 	virtual void fileOpen() = 0;
 	virtual void fileClose() = 0;
-	virtual void execute(int lba, int data = 0) = 0;
-private:
-
+	virtual void execute(int lba, string data ="") = 0;
+	const string WRITEFILENAME = "nand.txt";
+	const string READFILENAME = "result.txt";
 };
--- ssd/ReadCmdHandler.cpp
+++ ssd/ReadCmdHandler.cpp
@@ -2,5 +2,14 @@
 
 class ReadCmdHandler : public CmdHandler
 {
-
+	// CmdHandler을(를) 통해 상속됨
+	void fileOpen() override
+	{
+	}
+	void fileClose() override
+	{
+	}
+	void execute(int lba, string data) override
+	{
+	}
 };
