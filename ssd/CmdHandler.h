e2e0b3ace72e41de1290d3f9e92ada1bcece3b01
juwon.kim <saylily90@naver.com>
2024-07-08 13:46:01 +0900

    [feature] Added CmdHandler basic files 1. 상태 (feature) 2. 수정/반영내용 : added SDD manager to file open, close, execute command gitmessage added use git bash command "git config --global commit.template .gitmessage.txt" 3. 추가된 (Test된 ) TC 명 : X unit test 전체 Pass 여부

--- ssd/CmdHandler.h
+++ ssd/CmdHandler.h
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-
 class CmdHandler
 {
-
+public:
+	virtual void fileOpen() = 0;
+	virtual void fileClose() = 0;
+	virtual void execute(int lba, int data = 0) = 0;
+private:
 
 };
--- ssd/SSDManager.cpp
+++ ssd/SSDManager.cpp
@@ -2,7 +2,19 @@
 
 class SSDManager
 {
-
+	SSDManager(CmdHandler *handler)
+		: cmdHandler(handler)
+	{
+		cmdHandler->fileOpen();
+	}
+	~SSDManager()
+	{
+		cmdHandler->fileClose();
+	}
+	void runCommand()
+	{
+		cmdHandler->execute();
+	}
 private:
 	CmdHandler* cmdHandler;
 };
