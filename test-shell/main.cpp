0ff67d58cd04b66da292b106f2d5071fe07795d2
minho.kim <mh2022.kim@samsung.com>
2024-07-09 15:37:49 +0900

    [hotfix] make test-shell can run

--- test-shell/main.cpp
+++ test-shell/main.cpp
@@ -1,3 +1,20 @@
-int main() {
+#include "shell.h"
+#include "command_factory.cpp"
+#include "ReadCommand.cpp"
+#include "WriteCommand.cpp"
+#include "ssdlib.cpp"
 
+int main() {
+	ICommandFactory& factory = CommandFactory::getInstance();
+	std::string path = "\\..\\x64\\Debug";
+	std::string app_name = "\\ssd.exe";
+	std::string result_path = "";
+	ISSD* ssd = new SSDLib(path, app_name, result_path);
+	factory.injectCommand("read", new ReadCommand(*ssd));
+	factory.injectCommand("write", new WriteCommand(*ssd));
+	factory.injectCommand("fullread", new FullReadCommand(*ssd));
+	factory.injectCommand("fullwrite", new FullwriteCommand(*ssd));
+
+	Shell* shell = new Shell(factory);
+	shell->run();
 }
