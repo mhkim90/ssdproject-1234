892f2e47be82205c86ada430dd1b51f1856dcd6e
changhyun-cra <hellow6044@gmail.com>
2024-07-09 10:19:09 +0900

    [feature] Add Full Read Command 1. 상태: feature 2. 수정 내용: Full Read Execute 함수 구현 3. 추가된 (Test된 ) TC 명: Shell_FullRead_Execute_Success 4. Unit Test 전체 Pass 여부: O

--- unit-test/test_shell_read.cpp
+++ unit-test/test_shell_read.cpp
@@ -7,7 +7,6 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 using namespace testing;
@@ -43,6 +42,31 @@ protected:
 	}
 };
 
+class FullReadCommandFixture : public testing::Test {
+public:
+	SSDMock ssdMock;
+	FullReadCommand command{ ssdMock };
+	CommandArgs normalArgs, abnormalArgs;
+
+	const int Success_LBA = 99;
+	const int Fail_LBA = 101;
+	const string TEST_DATA = "0x12345678";
+	const string strHelp = "\
+		LBA에 적힌 값을 읽어 화면에 출력한다.\n \
+		[Example] read LBA\n \
+		[Parameters]\n \
+		- LBA: 값을 읽을 LBA 영역 값 (0~99)\n \
+		[Returns] LBA에서 읽은 데이터를 출력합니다.\n";
+
+private:
+
+protected:
+	void SetUp() override {
+		normalArgs = {  };
+		abnormalArgs = {  };
+	}
+};
+
 TEST_F(ReadCommandFixture, Shell_Read_Execute_Success) {
 	EXPECT_CALL(ssdMock, read(Success_LBA))
 		.Times(1)
@@ -58,4 +82,14 @@ TEST_F(ReadCommandFixture, Shell_Read_Execute_Fail) {
 
 TEST_F(ReadCommandFixture, Shell_Read_GetHelp) {
 	EXPECT_EQ(strHelp, command.getHelp());
+}
+
+TEST_F(FullReadCommandFixture, Shell_FullRead_Execute_Success) {
+	for (int i = 0; i < 100; i++) {
+		EXPECT_CALL(ssdMock, read(i))
+			.Times(1)
+			.WillOnce(Return("i"));
+	}
+
+	command.execute(normalArgs);
 }
