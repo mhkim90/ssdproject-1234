abe10e69898a3a862239a4d619ac10e8e3639e0c
juheon.song <bootplus@naver.com>
2024-07-10 08:03:32 +0900

    [feature] CommandBase method 추가

--- test-shell/command.h
+++ test-shell/command.h
@@ -18,16 +18,25 @@ interface ICommand {
 
 class CommandBase : public ICommand {
 public:
-	CommandBase(int verifyArgsCount = 0);
+	CommandBase(ISSD& ssd, const string&  helpMsg, int verifyArgsCount = 0);
 
-	virtual void injectSSD(ISSD& ssd) = 0;
 	virtual void execute(const vector<string>& args) = 0;
-	virtual const string& getHelp() = 0;
+
+	virtual void injectSSD(ISSD& ssd) override;
+	virtual const string& getHelp() override;
 
 protected:
-	void verifyArgsCount(const vector<string>& args) const;
-	void verifyAddress(const string& arg) const;
-	void verifyValue(const string& arg) const;
+	inline void verifyArgsCount(const vector<string>& args) const;
+	inline void verifyFormatAddress(const string& arg) const;
+	inline void verifyFormatValue(const string& arg) const;
+	inline void verifyAddressRange(int addr) const;
+	inline ISSD& getSSD() const;
 
+private:
+	static constexpr int ADDR_RANGE_MIN = 0;
+	static constexpr int ADDR_RANGE_MAX = 99;
+
+	ISSD& _ssd;
 	const int _VERIFY_ARGS_COUNT;
+	const string _HELP_MSG;
 };
