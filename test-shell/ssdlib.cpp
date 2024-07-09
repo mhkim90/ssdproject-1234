3f84f3842534cd556f7acce95203f9c7f49ec9a7
minho.kim <mh2022.kim@samsung.com>
2024-07-09 14:39:02 +0900

    [refactoring] fix ssdlib app launch failure

--- test-shell/ssdlib.cpp
+++ test-shell/ssdlib.cpp
@@ -7,33 +7,48 @@
 class SSDLib : public ISSD {
 public:
 	SSDLib(const std::string& directory_path)
-		: directory_path { directory_path } {}
+		: directory_path { directory_path } {
+		GetCurrentDirectoryA(256, current_path);
+	}
+
+	SSDLib(const std::string& directory_path,
+		const std::string& app_name)
+		: directory_path{ directory_path }
+		, app_name{ app_name } {
+		GetCurrentDirectoryA(256, current_path);
+	}
+
+	SSDLib(const std::string& directory_path,
+		const std::string& app_name,
+		const std::string& result_path)
+		: directory_path{ directory_path }
+		, app_name{ app_name } 
+		, result_path{ result_path } {
+		GetCurrentDirectoryA(256, current_path);
+	}
 
 	void write(int addr, const std::string& value) override {
-		std::string command = directory_path + "\\" + app_name + " W " + 
+		std::string command = std::string(current_path) + 
+			directory_path + app_name + " W " +
 			std::to_string(addr) + " " + value;
-		ShellExecuteA(NULL, "open", command.c_str(), NULL, NULL, 0);
+		system(command.c_str());
 	}
 
 	std::string read(int addr) override {
-		std::string command = directory_path + "\\" + app_name + " R " + 
+		std::string command = std::string(current_path) +
+			directory_path + app_name + " R " +
 			std::to_string(addr);
-		ShellExecuteA(NULL, "open", command.c_str(), NULL, NULL, 0);
-		return getValue(directory_path + "\\" + result_name);
-	}
-
-	void setSSDApp(const std::string& app_name) {
-		this->app_name = app_name;
-	}
-
-	void setResultName(const std::string& result_name) {
-		this->result_name = result_name;
+		system(command.c_str());
+		return getValue(std::string(current_path) + result_path +
+						result_name);
 	}
 
 private:
-	std::string directory_path;
-	std::string app_name = "ssd.exe";
-	std::string result_name = "result.txt";
+	char current_path[256];
+	std::string directory_path = "\\..\\x64\\Debug";
+	std::string app_name = "\\ssd.exe";
+	std::string result_path = directory_path;
+	std::string result_name = "\\result.txt";
 
 	std::string getValue(const std::string& result_path) {
 		std::ifstream result(result_path);
