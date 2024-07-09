4f953c741ee011bb5dfb2243948956654eee9901
seulgiishin <tls940807@gmail.com>
2024-07-09 11:11:49 +0900

    [refactoring] ssd refactoring - add FileManager & command refactoring 1. 상태 (refactoring) 2. 수정/반영내용 - add FileManager.cpp - Refactoring Read/Write command using FileManager 3. 추가된 (Test된 ) TC 명 4. Unit Test TC all pass

--- ssd/WriteCmdHandler.cpp
+++ ssd/WriteCmdHandler.cpp
@@ -9,10 +9,6 @@ public:
 	WriteCmdHandler() {
 
 		CmdHandler::setOpcode(CmdOpcode::WRITE_CMD);
-
-		cout << "File Init\n";
-
-		initFile();
 	}
 
 	bool sanityCheckPassed(int lba, string data) override
@@ -27,65 +23,13 @@ public:
 		}
 	}
 
-	// set file as zero
-	void initFile()
-	{
-		writeFile.open(WRITEFILENAME);
-		string tmp = "0x00000000\n";
-		if (writeFile.is_open())
-		{
-			for (int i = 0; i < 100; i++) {
-				writeFile << "0x00000000\n";
-			}
-		}
-
-		writeFile.close();
-	}
-
 	// CmdHandler을(를) 통해 상속됨
 	void execute(int lba, string data) override
 	{
-		fileOpen();
+		FileManager::getInstance().openNand();
 
-		buf[lba] = data;
+		FileManager::getInstance().updateNand(lba, data);
 
-		fileClose();
+		FileManager::getInstance().writeNand();
 	}
-
-	void fileOpen() override
-	{
-		// 파일 읽기 준비
-		std::ifstream in(WRITEFILENAME);
-		string tmp;
-
-		if (!in.is_open()) {
-			std::cout << "파일을 찾을 수 없습니다!" << std::endl;
-			return;
-		}
-		int i = 0;
-		while (getline(in, tmp, '\n')){
-			buf[i++] = tmp;
-		}
-
-		in.close();
-	}
-
-	void fileClose() override
-	{
-		writeFile.open(WRITEFILENAME);
-		if (writeFile.is_open())
-		{
-			for (int i = 0; i < 100; i++) {
-				writeFile.write((buf[i] + "\n").c_str(), (buf[i] + "\n").size());
-			}
-		}
-
-		cout << "File Close\n";
-
-		writeFile.close();
-	}
-
-private:
-	ofstream writeFile;
-	string buf[100];
 };
