2e9f9af733d91615f498c620b28ef4f08849ddae
minho.kim <mh2022.kim@samsung.com>
2024-07-10 11:38:17 +0900

    [feature] add print on Logger with Test

--- logger/Logger.cpp
+++ logger/Logger.cpp
@@ -2,9 +2,16 @@
 #include <filesystem>
 #include <string>
 #include <iostream>
+#include <chrono>
+#include <sstream>
+#include <fstream>
 
 Logger::Logger(const std::string& module_name) {
 	m_module_name = module_name;
+	if (std::filesystem::exists(m_module_name)) {
+		std::filesystem::remove_all(m_module_name);
+	}
+	std::filesystem::create_directory(m_module_name);
 }
 
 Logger& Logger::getInstance(const std::string& module_name) {
@@ -12,8 +19,54 @@ Logger& Logger::getInstance(const std::string& module_name) {
 	return instance;
 }
 
-void Logger::writeLog(
+void Logger::printLog(PRINT_TYPE type,
 	const std::string& func_name,
 	const std::string& message) {
+	std::string output = getTime() + expandName(func_name) + ":"
+		+ message;
+	switch (type) {
+		case PRINT_TYPE::FILE:
+			saveLog(output);
+			break;
+		case PRINT_TYPE::CONSOLE:
+			std::cout << output << std::endl;
+			break;
+		case PRINT_TYPE::ALL:
+			std::cout << output << std::endl;
+			saveLog(output);
+			break;
+		default:
+			break;
+	}
+}
 
+void Logger::saveLog(
+	const std::string& output) {
+	std::ofstream file;
+	file.open(m_module_name + "\\latest.log",
+		std::ios::out | std::ios::app);
+	file << output << "\n";
+	file.close();
+}
+
+std::string Logger::expandName(const std::string& func_name) {
+	if (func_name.size() < m_name_buffer) {
+		std::string expanded_func_name = func_name;
+		for (size_t i = 0; i < m_name_buffer - func_name.size(); i++) {
+			expanded_func_name.append(" ");
+		}
+		return expanded_func_name;
+	}
+	return func_name;
+}
+
+std::string Logger::getTime() {
+    time_t current_time_t = std::chrono::system_clock::to_time_t(
+		std::chrono::system_clock::now());
+    tm current_time;
+    localtime_s(&current_time, &current_time_t);
+	std::stringstream converted_time;
+	converted_time << std::put_time(
+		&current_time, "[%Y.%m.%d %H:%M] ");
+	return converted_time.str();
 }
--- unit-test/test_logger.cpp
+++ unit-test/test_logger.cpp
@@ -1,1 +1,18 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "../logger/Logger.h"
+
+TEST(LoggerTest, CoutTest) {
+	std::string module_name = "gtest";
+	ILogger& logger = Logger::getInstance(module_name);
+	std::string name = "func()";
+	std::string msg = "test message";
+	const int m_name_buffer = 30;
+	testing::internal::CaptureStdout();
+
+	logger.printLog(PRINT_TYPE::CONSOLE, name, msg);
+	std::string captured_out = testing::internal::GetCapturedStdout();
+	int begin_idx = captured_out.find_last_of(")");
+	int end_idx = captured_out.find_last_of(":");
+	EXPECT_EQ(end_idx - begin_idx - 1, m_name_buffer - name.size());
+}
