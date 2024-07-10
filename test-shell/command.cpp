3974939850b8f5ba95f163933e932b98e28c517d
bootplus <bootplus@naver.com>
2024-07-10 21:16:47 +0900

    [refactoring] h/cpp  분리 CommandBase, ScriptBase

--- test-shell/command.cpp
+++ test-shell/command.cpp
@@ -1,50 +1,64 @@
 #include <regex>
 #include <stdexcept>
 #include "command.h"
-//
-//CommandBase::CommandBase(ISSD& ssd, const string&  helpMsg, int verifyArgsCount)
-//	: _ssd { ssd }
-//	, _HELP_MSG { helpMsg }
-//	, _VERIFY_ARGS_COUNT { verifyArgsCount }
-//{
-//}
-//
-//void CommandBase::injectSSD(ISSD& ssd)
-//{
-//	_ssd = ssd;
-//}
-//
-//const string& CommandBase::getHelp()
-//{
-//	return _HELP_MSG;
-//}
-//
-//void CommandBase::verifyArgsCount(const vector<string>& args) const
-//{
-//	if ((int)args.size() < _VERIFY_ARGS_COUNT) throw std::invalid_argument("Not enough arguments. Check help.");
-//}
-//
-//void CommandBase::verifyFormatAddress(const string& arg) const
-//{
-//	static std::regex REGEX{ "^[0-9]$" };
-//	if (std::regex_match(arg, REGEX)) return;
-//	throw std::invalid_argument("Invalid arguments. Check help.");
-//}
-//
-//void CommandBase::verifyFormatValue(const string& arg) const
-//{
-//	static std::regex REGEX{ "^0x[A-Z]{8}$" };
-//	if (std::regex_match(arg, REGEX)) return;
-//	throw std::invalid_argument("Invalid arguments. Check help.");
-//}
-//
-//inline void CommandBase::verifyAddressRange(int addr) const
-//{
-//	if (ADDR_RANGE_MIN <= addr && addr <= ADDR_RANGE_MAX) return;
-//	throw std::invalid_argument("Invalid arguments. Check help.");
-//}
-//
-//ISSD& CommandBase::getSSD() const
-//{
-//	return _ssd;
-//}
+
+CommandBase::CommandBase(ISSD& ssd, int verifyArgsCount)
+	: _ssd{ ssd }
+	, _VERIFY_ARGS_COUNT{ verifyArgsCount }
+	, logger{ Logger::getInstance("shell") }
+{
+}
+
+void CommandBase::injectSSD(ISSD& ssd)
+{
+	_ssd = ssd;
+}
+
+void CommandBase::verifyArgsCount(const vector<string>& args) const
+{
+	if ((int)args.size() >= _VERIFY_ARGS_COUNT) return;
+	throw std::invalid_argument("Not enough arguments. Check help.");
+}
+
+void CommandBase::verifyFormatAddress(const string& arg) const
+{
+	static std::regex REGEX{ "^[0-9]+$" };
+	if (std::regex_match(arg, REGEX)) return;
+	throw std::invalid_argument("Invalid arguments. Check help.");
+}
+
+void CommandBase::verifyFormatValue(const string& arg) const
+{
+	static std::regex REGEX{ "^0x[0-9A-Z]{8}$" };
+	if (std::regex_match(arg, REGEX)) return;
+	throw std::invalid_argument("Invalid arguments. Check help.");
+}
+
+void CommandBase::verifyAddressRange(int addr) const
+{
+	if (_ADDR_RANGE_MIN <= addr && addr <= _ADDR_RANGE_MAX) return;
+	throw std::invalid_argument("Invalid arguments. Check help.");
+}
+
+ISSD& CommandBase::getSSD() const
+{
+	return _ssd;
+}
+
+ScriptBase::ScriptBase(ISSD& ssd, const string& scriptName)
+	: CommandBase(ssd)
+	, _SCRIPT_NAME{ scriptName }
+{
+}
+
+void ScriptBase::printRun() const
+{
+	std::cout << _SCRIPT_NAME << " --- Run...";
+}
+
+void ScriptBase::printResult(bool isPass)
+{
+	std::cout << (isPass ? "Pass" : "FAIL!") << std::endl;
+	if (isPass) return;
+	throw std::logic_error("Faild of Script!");
+}
--- unit-test/test_main.cpp
+++ unit-test/test_main.cpp
@@ -1,24 +1,5 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
-#include <iostream>
-#include <string>
-#include <vector>
 
-using namespace std;
-using namespace testing;
-
-class SSDFixture : public testing::Test {
-public:
-
-private:
-
-protected:
-	void SetUp() override {
-
-	}
-};
-
-TEST_F(SSDFixture, basicWriteTest) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
-}
+/*Test target cpp files*/
+#include "../test-shell/command.cpp"
